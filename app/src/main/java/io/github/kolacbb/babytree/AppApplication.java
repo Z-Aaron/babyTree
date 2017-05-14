b340b6c36240b1496adf192cd87819694a368a4e
kolacbb <kolacbb@gmail.com>
2017-05-14 23:06:18 +0800

    circle partment

--- app/src/main/java/io/github/kolacbb/babytree/AppApplication.java
+++ app/src/main/java/io/github/kolacbb/babytree/AppApplication.java
@@ -3,6 +3,8 @@ package io.github.kolacbb.babytree;
 import android.app.Application;
 import android.preference.PreferenceManager;
 
+import com.tencent.bugly.crashreport.CrashReport;
+
 import io.github.kolacbb.babytree.util.SpUtils;
 
 /**
@@ -13,6 +15,10 @@ public class AppApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // 腾讯 bugly
+        CrashReport.initCrashReport(getApplicationContext(), "25ac536135", false);
+
         SpUtils.init(PreferenceManager.getDefaultSharedPreferences(this));
     }
 }
---
+++ app/src/main/java/io/github/kolacbb/babytree/ui/activity/UserCircleActivity.java
@@ -0,0 +1,21 @@
+package io.github.kolacbb.babytree.ui.activity;
+
+import android.os.Bundle;
+
+import io.github.kolacbb.babytree.R;
+import io.github.kolacbb.babytree.base.BaseActivity;
+
+/**
+ * Created by kolab on 2017/5/7.
+ */
+
+public class UserCircleActivity extends BaseActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_circle);
+    }
+
+
+}
